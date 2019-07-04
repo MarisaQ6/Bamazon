23f7b5f0e44a5c227905690aaeb63691136fa446
Marisa Quijano <miss.quijano@gmail.com>
2019-07-03 20:37:13 -0400

    adding cust js

--- bamazonCust.js
+++ bamazonCust.js
@@ -33,3 +33,46 @@ function showItems(){
 function question(){
     
 }
+
+prompt.start();
+
+prompt.get(["id", "inStock"], function (err, result) {
+	if (err){
+	    console.log(err)
+	}
+
+	var CustomerPickID = parseInt(result.id);
+	var CustomerQuantity = parseInt(result.howMany);
+
+	console.log("id=" + CustomerPickID, "in stock =" + CustomerQuantity);
+
+	function selectID(){
+		connection.query('SELECT * FROM Products WHERE ItemID =' + CustomerPickID, function(err, res) { 
+			if (err) throw err;
+			console.log(res);
+			var want = CustomerQuantity;
+   			var have = res[0].StockQuantity;
+   			var individPrice = res[0].Price;
+   			newQuantity = have - want;
+   			if (newQuantity >= 0){
+          		console.log('we have stock '+res[0].ProductName+' in stock.');
+          		var totalCost= individPrice * CustomerQuantity;
+          		console.log("The total is " +totalCost);
+          		connection.query('UPDATE Products SET StockQuantity = '+newQuantity+' WHERE ItemID ='+CustomerPickID, function(err, res){
+        		if (err) throw err;
+        			connection.query('SELECT ItemID, ProductName, DepartmentName, Price, StockQuantity FROM products WHERE ItemID ='+CustomerPickID, function(err, res){
+          				console.log(res);
+          			});	
+        		});
+          	}	
+   			else if(want>have && have !=0){
+			    console.log('Our apologies. We only have '+have+' in stock.');
+		  	}
+		  	else{
+	          return false
+	        }
+		});
+	};
+	selectID();	
+	});
+// });
